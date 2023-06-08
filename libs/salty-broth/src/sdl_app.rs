865910762be9b32e88402436a4d89e0e385675b4
Andrea Rossini <rossini.andrea.lc@gmail.com>
2023-06-08 19:29:13 +0200

    Simplify sdl-dispatch before building an asyng UI toolkit.

--- libs/salty-broth/src/sdl_app.rs
+++ libs/salty-broth/src/sdl_app.rs
@@ -1,3 +1,17 @@
+use sdl2::{
+    pixels::{Color, PixelFormatEnum},
+    event::{Event, WindowEvent},
+    keyboard::Keycode,
+    render::Canvas,
+    video::Window,
+};
+use sdl_dispatch::SdlDispatcher;
+use std::{
+    iter,
+    sync::Arc,
+};
+use tokio::runtime::Runtime;
+
 macro_rules! dispatch_handlers {
     (
         $app,
@@ -35,33 +49,35 @@ macro_rules! dispatch_message {
     }
 }
 
-trait App {
+pub trait App {
     type DispatchMessages;
 
+    fn window_info(&self) -> (String, u32, u32);
     fn resized(&self);
     fn canvas_ready(&self, canvas: Canvas<Window>);
-    fn handle_dispatch(&self, message: Self::DispatchMessages);
+    fn handle_dispatch(&self, message: &Self::DispatchMessages);
 }
 
-static mut ui_dispatcher: Option<SdlDispatcher> = None;
+static mut ui_dispatcher: Option<Arc<SdlDispatcher>> = None;
 
-pub fn dispatcher() -> SdlDispatcher {
-    ui_dispatcher.clone()
+pub fn dispatcher() -> Arc<SdlDispatcher> {
+    ui_dispatcher.expect("Trying to use the dispatcher without a running app.").clone()
 }
 
 pub fn run<T>(app: T) 
-where T: App {
+where T: App, <T as App>::DispatchMessages: Send + Clone {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let event_subsystem = sdl_context.event().unwrap();
-     
-    let window = video_subsystem.window("Mandelbrot Explorer", 800, 600)
+
+    let (title, w, h) = app.window_info();
+    let window = video_subsystem.window(&title, w, h)
         .resizable()
         .opengl()
         .build()
         .unwrap();
     let mut canvas = window.into_canvas().build().unwrap();
-    let (ui_executor, disp) = pumptasks::new_executor_and_dispatcher::<T::DispatchMessages, ()>(&event_subsystem);
+    let (ui_executor, disp) = sdl_dispatch::new_executor_and_dispatcher::<T::DispatchMessages, ()>(&event_subsystem);
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     ui_dispatcher = Some(disp);
@@ -100,7 +116,7 @@ where T: App {
         }
         
         if resized {
-            self.app.resized();
+            app.resized();
         }
     }
 }
--- libs/sdl-dispatch/src/lib.rs
+++ libs/sdl-dispatch/src/lib.rs
@@ -1,3 +1,144 @@
-mod sdl_backend;
-pub use crate::sdl_backend::*;
+use sdl2::event::{Event, EventSender};
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{waker_ref, ArcWake},
+};
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    task::{Context, Poll, Waker},
+};
+
+/// Task executor that receives task notifications from SDL messages.
+pub struct SdlExecutor/*<TIn, TOut>*/ {
+
+}
+
+/// `SdlDispatcher` spawns new futures onto the SDL message pump.
+#[derive(Clone)]
+pub struct SdlDispatcher/*<TIn, TOut>*/ {
+    event_sender: Arc<EventSender>,
+}
+
+struct SharedState<TOut: 'static + Send + Clone> {
+    completed: Poll<TOut>,
+    waker: Option<Waker>,
+}
+
+/// A future that gets scheduled in SDL pump
+pub struct SdlPumpTask<TIn: 'static + Send + Clone, TOut: 'static + Send + Clone> {
+    /// Input data
+    input: TIn,
+
+    shared_state: Arc<Mutex<SharedState<TOut>>>,
+}
+
+impl<TIn: 'static + Send + Clone, TOut: 'static + Send + Clone> SdlPumpTask<TIn, TOut> {
+    pub fn input(&self) -> &TIn {
+        &self.input
+    }
+
+    pub fn complete(&self, result: TOut) {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        shared_state.completed = Poll::Ready(result);
+
+        if let Some(waker) = shared_state.waker.take() {
+            waker.wake()
+        }
+    }
+}
+
+/// A future for the awaiter side
+pub struct Task<TOut: 'static + Send + Clone> {
+    shared_state: Arc<Mutex<SharedState<TOut>>>,
+}
+
+impl SdlDispatcher/*<TIn, TOut>*/ {
+    fn new(sdl_events: &sdl2::EventSubsystem) -> Self {
+        Self {
+            event_sender: sdl_events.event_sender().into(),
+        }
+    }
+
+    pub fn spawn<TIn: 'static + Send + Clone, TOut: 'static + Send + Clone>(&self, input: TIn) -> Task<TOut> {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: Poll::Pending,
+            waker: None,
+        }));
+        let task = Arc::new(SdlPumpTask {
+            input: input,
+            shared_state: shared_state.clone(),
+        });
+
+        self.event_sender.push_custom_event::<Arc<SdlPumpTask<TIn, TOut>>>(task)
+            .expect("Can't push on SDL pump");
+        Task::<TOut> {
+            shared_state: shared_state.clone(),
+        }
+    }
+}
+
+/*
+impl ArcWake for SdlPumpTask {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        // Implement `wake` by sending this task back onto the task channel
+        // so that it will be polled again by the executor.
+        let cloned = arc_self.clone();
+        arc_self.event_sender.push_custom_event::<Arc<SdlPumpTask>>(cloned)
+            .expect("Can't push on SDL pump");
+    }
+}*/
+
+impl SdlExecutor {
+    /// Handles an SDL event, running a task if it is a Task Notification
+    /// Returns `true` if the event was handled.
+    pub fn handle_sdl_event<TIn: 'static + Send + Clone, TOut: 'static + Send + Clone>(&self, event: &Event) -> Option<Arc<SdlPumpTask<TIn, TOut>>> {
+        if !event.is_user_event() {
+            return None;
+        }
+
+        event.as_user_event_type::<Arc<SdlPumpTask<TIn, TOut>>>()
+        /*{
+            Some(notification) => {
+                let mut future_slot = notification.future.lock().unwrap();
+                if let Some(mut future) = future_slot.take() {
+                    let waker = waker_ref(&notification);
+                    let context = &mut Context::from_waker(&waker);
+
+                    if future.as_mut().poll(context).is_ready() {
+                        let mut shared_state = notification.shared_state.lock().unwrap();
+                        shared_state.completed = true;
+
+                        if let Some(waker) = shared_state.waker.take() {
+                            waker.wake();
+                        }
+                    }
+                }
+                true
+            }
+            None => false
+        }*/
+    }
+}
+
+impl<TOut: 'static + Send + Clone> Future for Task<TOut> {
+    type Output = TOut;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // Look at the shared state to see if the timer has already completed.
+        let mut shared_state = self.shared_state.lock().unwrap();
+
+        if shared_state.completed.is_pending() {
+            shared_state.waker = Some(cx.waker().clone());
+        }
+
+        shared_state.completed.clone()
+    }
+}
+
+pub fn new_executor_and_dispatcher<TIn: 'static + Send + Clone, TOut: 'static + Send + Clone>(sdl_events: &sdl2::EventSubsystem) -> (SdlExecutor, SdlDispatcher) {
+    sdl_events.register_custom_event::<Arc<SdlPumpTask<TIn, TOut>>>();
+    (SdlExecutor {}, SdlDispatcher::new(sdl_events))
+}
 
